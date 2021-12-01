9e7a8d94334efb7a2fde8d3a53716fe65e33508e
fridom99 <92981202+fridom99@users.noreply.github.com>
2021-12-01 09:43:35 +0100

    Add files via upload

---
+++ data.php
@@ -0,0 +1,18 @@
+<?php
+
+var_dump($_POST);
+$nom = $_POST['nom'];
+$firstname = $_POST['fisrtname'];
+$age = $_POST['age'];
+$ville = $_POST['ville'];
+
+
+// if(isset($nom) && isset($firstname) && isset($age) && isset($ville)){
+if(($nom == null) && ($firstname == null) && ($age == null) && ($ville == null)){
+  echo (" y'a une couille dans le paté");
+
+} else {
+  echo("Les champs sont remplit");
+}
+
+?>
